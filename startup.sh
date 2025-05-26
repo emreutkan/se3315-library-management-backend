c551cefd921c175de0ce83a4c7d4be0418b46ad7
Irfan Emre Utkan <127414322+emreutkan@users.noreply.github.com>
2025-05-26 08:57:06 +0300

    refactor: update application entry point and add Gunicorn to requirements

--- startup.sh
+++
@@ -1,8 +0,0 @@
-# startup.sh - Azure App Service startup command
-# This file instructs Azure how to start your Flask application
-
-# First, import the configuration to set environment variables
-python azure_config.py
-
-# Start the Flask app using Gunicorn
-gunicorn --config gunicorn.conf.py wsgi:app
